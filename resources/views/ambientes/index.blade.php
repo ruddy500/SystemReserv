33108739e033feb4a14de2dd9e8f4d431a3b4b4b
MadaiFlores <madaifloresmodragon@gmail.com>
2024-03-28 19:23:40 -0400

    tamanio texto 1.4

--- resources/views/ambientes/index.blade.php
+++ resources/views/ambientes/index.blade.php
@@ -8,8 +8,19 @@
 					<button type="button" class="btn btn-custom" data-bs-toggle="modal" data-bs-target="#formularioAmbiente" data-bs-whatever="@mdo"><i class="bi bi-plus-circle-fill"></i>  Registrar ambiente</button>
 					@include('ambientes.ambiente.registrar')
 
-
-
+					<div class="table-responsive">
+						<table class="table table-striped table-hover table-bordered">
+							<thead>
+								<tr>
+									<th class="text-center h4">Habilitar</th>
+									<!--<th class="text-center">Nombre de usuario</th>-->
+									<th class="text-center h4">Nombre de ambiente</th>
+									<th class="text-center h4">Capacidad</th>
+									<th class="text-center h4">Opciones</th>
+								</tr>
+							</thead>						
+						</table>
+					</div>
 					
 				</div>
 			</div>
