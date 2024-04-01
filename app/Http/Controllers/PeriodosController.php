ac3cd6dcee58d23d51a9663faa98bb38f2f8bf22
jonathanalexiiz <jonathanasda@gmail.com>
2024-04-01 13:29:50 -0400

    Controladores

---
+++ app/Http/Controllers/PeriodosController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Periodos;
+use Illuminate\Http\Request;
+
+class PeriodosController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Periodos $periodos)
+    {
+        //
+    }
+
+    public function edit(Periodos $periodos)
+    {
+        //
+    }
+
+    public function update(Request $request, Periodos $periodos)
+    {
+        //
+    }
+
+    public function destroy(Periodos $periodos)
+    {
+        //
+    }
+}
