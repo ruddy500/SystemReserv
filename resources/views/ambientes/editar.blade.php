6458a17c569cfaadaeeb531649b6625acae2eb67
MadaiFlores <madaifloresmodragon@gmail.com>
2024-04-03 19:28:54 -0400

    editar ventana

--- resources/views/ambientes/editar.blade.php
+++ resources/views/ambientes/editar.blade.php
@@ -4,7 +4,7 @@
 
     <div class="container">
         <div class="row">
-            <h1>ESTO ES UNA PRUEBA PARA VERIFICAR LA REDIRECCION DE EDITAR EJEJJE</h1>
+            <h1>ESTO ES UNA PRUEBA PARA VERIFICAR LA REDIRECCION DE EDITAR EJEJJE y ver la ventana</h1>
         </div>
     </div>
 
