0131f736165bbcac711f09a52cd4389a4c8037d9
MadaiFlores <madaifloresmodragon@gmail.com>
2024-04-02 23:19:00 -0400

    posicion boton anadir

--- resources/views/ambientes/horario.blade.php
+++ resources/views/ambientes/horario.blade.php
@@ -9,19 +9,19 @@
                     <div class="col">
                         <label for="dia-name" class="col-form-label h4">Día:</label>
                         <select class="form-select" aria-label="Default select example">
-                        <option selected>Seleccione dia</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                            <option selected>Seleccione dia</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
                         </select>
                     </div>
                     <div class="col">
                         <label for="horario-name" class="col-form-label h4">Horario:</label>
                         <select class="form-select" aria-label="Default select example">
-                        <option selected>Seleccione horario</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                            <option selected>Seleccione horario</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
                         </select>
                     </div>
                 </div>
