d25b4d44f3b5edb77827cb33f833d8a0de12e7ff
MadaiFlores <madaifloresmodragon@gmail.com>
2024-04-02 23:52:43 -0400

    prueba tabla horario

--- resources/views/ambientes/horario.blade.php
+++ resources/views/ambientes/horario.blade.php
@@ -30,7 +30,37 @@
                         <button type="button" class="btn btn-horario margin-end" data-bs-target="#" data-bs-whatever="@mdo">Añadir</button>
                     </div>
                 </div>
+                <!-- PRUEBA DE TABLA DE HORARIOS DISPONIBLES -->
+                <label for="tablehorario-name" class="col-form-label h4">Horarios disponibles:</label>
+                <table id="horario-tabla" class="table caption-top">
+                    <thead>
+                        <tr>
+                        <th scope="col">Dia</th>
+                        <th scope="col">Horario</th>
+                        <th scope="col">Estado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>Lunes</td>
+                        <td>06:45-08:15</td>
+                        <td>Libre</td>
+                        </tr>
 
+                        <tr>
+                        <td>Lunes</td>
+                        <td>15:45-17:15</td>
+                        <td>Libre</td>
+                        </tr>
+
+                        <tr>
+                        <td>Martes</td>
+                        <td>Todos los horarios</td>
+                        <td>Libre</td>
+                        </tr>
+                    </tbody>
+                </table>
+            
             </div>
         </div>
 </div>
