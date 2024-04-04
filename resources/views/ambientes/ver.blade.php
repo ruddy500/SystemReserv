fc6c3ab40354110929303821a6956e2e576a635a
MadaiFlores <madaifloresmodragon@gmail.com>
2024-04-03 21:38:36 -0400

    ver ambiente

--- resources/views/ambientes/ver.blade.php
+++ resources/views/ambientes/ver.blade.php
@@ -1,11 +1,18 @@
 @extends('index')
 
 @section('ambientes/ver')
-
-    <div class="container">
-        <div class="row">
-            <h1>ESTO ES UNA PRUEBA PARA VERIFICAR LA REDIRECCION DE VER</h1>
+<div class="container mt-3">
+		<div class="card vercard">
+			<h3 class="card-header">Aqui poner nombre de ambiente</h3>
+            <div class="card-body bg-content">
+                <div class ="card details-card">
+                    <h4 class="card-header details-header">Detalle de ambiente</h3>
+                    <div class="card-body bg-content">
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+</div>        
+
 
 @endsection@
