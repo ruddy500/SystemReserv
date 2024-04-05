ed88723ac2cb9be15974616ffe1d26355ebd78be
AndruQM <quispemoyaa@gmail.com>
2024-04-05 08:35:19 -0400

    detalle horario anadidoo

--- resources/views/ambientes/ver.blade.php
+++ resources/views/ambientes/ver.blade.php
@@ -8,6 +8,30 @@
                 <div class ="card details-card">
                     <h4 class="card-header details-header">Detalle de ambiente</h3>
                     <div class="card-body bg-content">
+                        <div>
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr class="table_encabezado_color">
+                                        <td colspan="2">Detalle</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Nombre de Ambiente</td>
+                                        <td>Auditorio</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Capacidad</td>
+                                        <td>40</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">Descripción:
+                                            <br>
+                                            aqui se muestra las descripcion del ambiente
+                                        </td>
+                                    </tr>
+                                </tbody>                        
+                            </table>
+                        </div>
+
                     </div>
                 </div>
             </div>
