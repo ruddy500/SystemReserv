c810b7535fb9f42f455866a73941cb508c5cb6ce
MadaiFlores <madaifloresmodragon@gmail.com>
2024-04-15 16:13:50 -0400

    hoja docente

--- resources/views/reservas/principal.blade.php
+++ resources/views/reservas/principal.blade.php
@@ -3,7 +3,7 @@
 @section('inicio')
     <div class="container">
         <div class="row">
-            <h1>Inicio</h1>
+            <h1>Esta hoja es para la vista del docente en reservas</h1>
         </div>
     </div>
 @endsection
