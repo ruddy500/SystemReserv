d17d79c07ad5c08451fb7b3c8256e8095b4a35ac
Roger Espinoza <rudy800spinoza@gmail.com>
2024-04-15 20:32:47 -0400

    salirse uwu

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,6 +25,8 @@ class SessionsController extends Controller
     //para cerrar sesion
     public function destroy() {
 
-        
+        auth()->logout();
+
+        return redirect()->to('/');
     }
 }
