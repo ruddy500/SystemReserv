2280d66378bfeca977cabf19f846bf3c3bba9c93
MadaiFlores <madaifloresmodragon@gmail.com>
2024-04-18 17:48:46 -0400

    vista de ver

--- resources/views/reservas/ver.blade.php
+++ resources/views/reservas/ver.blade.php
@@ -8,6 +8,52 @@
                 <div class ="card details-card">
                     <h3 class="card-header details-header">Detalle de reserva</h3>                                    
                     <div class="card-body bg-content">
+                        <div>
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr class="table_encabezado_color">
+                                        <td colspan="2">Detalle</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 50%;">Nombre docente</td>
+                                        <td style="width: 50%;">Leticia Blanco</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 50%;">Nombre ambiente</td>
+                                        <td style="width: 50%;">690 A</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 50%;">Motivo de reserva</td>
+                                        <td style="width: 50%;">Examen primer parcial</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 50%;">Cantidad de estudiantes</td>
+                                        <td style="width: 50%;">200</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 50%;">Fecha</td>
+                                        <td style="width: 50%;">24-04-2024</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 50%;">Periodo</td>
+                                        <td style="width: 50%;">9:45-12:45</td>
+                                    </tr>
+                                </tbody>                        
+                            </table>
+                            <!-- TABLA DE DETALLE DE RESERVA -->
+                            <div class="table-responsive" style="max-height: 200px; overflow-y: auto;">
+                            <table id="horario-tabla" class="table caption-top">
+                                <thead class="table_encabezado_color text-center">
+                                    <td colspan="3">Detalle de materia</td>
+                                    <thead class="text-center">
+                                        <tr>
+                                            <th class="col">Materia</th>
+                                            <th scope="col">Grupo</th>
+                                        </tr>
+                                    </thead>
+                                </thead> 
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
