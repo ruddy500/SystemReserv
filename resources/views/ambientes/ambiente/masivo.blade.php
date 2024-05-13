fe442811d956fd78eeb53e4ef518024bd4d7134e
AndruQM <quispemoyaa@gmail.com>
2024-05-13 00:23:22 -0400

    modal de Exito de registro de ambientes masivos

--- resources/views/ambientes/ambiente/masivo.blade.php
+++ resources/views/ambientes/ambiente/masivo.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="modal-body">
                 <div class="container">
-                    <form action="{{route('import.excel')}}" method="post" enctype="multipart/form-data" class="needs-validation" novalidate>
+                    <form id="formulario-ambientes" action="{{route('import.excel')}}" method="post" enctype="multipart/form-data" class="needs-validation" novalidate>
                         @csrf
                         @include('componentes.validacion')
                         <div class="mb-3"> 
@@ -20,7 +20,8 @@
                         </div>
                     
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-aceptar">Aceptar</button>
+                            <!-- Agrega un evento onclick para mostrar la alerta de éxito -->
+                            <button type="button" class="btn btn-aceptar" onclick="enviarFormulario()">Aceptar</button>
                             <button type="button" class="btn btn-cancelar" data-bs-dismiss="modal">Cancelar</button>
                         </div>    
                     </form>
@@ -29,3 +30,39 @@
         </div>
     </div>
 </div>
+
+<!-- Script para enviar el formulario y mostrar la alerta de éxito -->
+<script>
+    function enviarFormulario() {
+    // Previene el comportamiento predeterminado del formulario
+    event.preventDefault();
+
+    // Muestra la alerta de éxito
+    Swal.fire({
+        icon: 'success',
+        title: '¡Éxito!',
+        text: 'Ambientes registrados exitosamente',
+        allowOutsideClick: false, // Evita que la alerta se cierre al hacer clic fuera de ella
+        showCancelButton: false, // Oculta el botón de cancelar
+        confirmButtonText: 'Aceptar' // Texto del botón de confirmación
+    }).then((result) => {
+        // Envía el formulario de manera asíncrona y redirige al usuario a la misma vista una vez que hagan clic en "OK"
+        if (result.isConfirmed) {
+            var form = document.getElementById('formulario-ambientes');
+            var formData = new FormData(form);
+            fetch(form.action, {
+                method: 'POST',
+                body: formData
+            }).then(response => {
+                if (response.ok) {
+                    window.location.href = '/ambientes'; // Reemplaza 'tu_url_aqui' con la URL de tu vista
+                } else {
+                    // Maneja el error
+                    console.error('Error:', response);
+                }
+            }).catch(error => console.error('Error:', error));
+        }
+    });
+}
+
+</script>
