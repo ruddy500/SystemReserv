f1ba521469a1b3aa2e349a70d1e619c132d11ccd
jonathanalexiiz <jonathanasda@gmail.com>
2024-05-19 19:41:30 -0400

    se arreglo lo de andres v.3

--- app/Http/Controllers/MensajesController.php
+++ app/Http/Controllers/MensajesController.php
@@ -9,12 +9,18 @@ class MensajesController extends Controller
     // Métodos del controlador
     public function enviarCorreo(Request $request)
     {
-        dd($request->all());
+        //aqui se esta recuperando el idReserva , los ambientes seleccionados 
+        //y un diferenciador que sera de tipo string con los valores de asignar , sugerir y rechazar
+        //aqui se haran las modificaciones con un if then else
+        //tambien se tendra que enviar a la vista un tipo de identificador para hacer 
+        //los diferentes tipos de mensaje
+        
+        // dd($request->all());
         $idReserva = $request->input('idReserva');
         
         $checkboxValue = $request->input('checkboxValues');
 
         $menu = view('componentes/menu'); // Crear la vista del menú
-        return view('mensajes.correo', compact('menu', 'idReserva',"checkboxValue"));
+        return view('mensajes.correo', compact('menu', 'idReserva','checkboxValue'));
     }
 }
