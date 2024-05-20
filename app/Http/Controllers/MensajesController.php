c56550de7e2bead5f9945457da9bc2c8a8c40b5b
Andres Bravo <BravoAndres@gmail.com>
2024-05-19 20:29:49 -0400

    actualizacionMensajes2

--- app/Http/Controllers/MensajesController.php
+++ app/Http/Controllers/MensajesController.php
@@ -9,42 +9,7 @@ use Illuminate\Http\Request;
 class MensajesController extends Controller
 {
     // Métodos del controlador
-    // public function enviarCorreo(Request $request)
-    // {
-    //     //aqui se esta recuperando el idReserva , los ambientes seleccionados 
-    //     //y un diferenciador que sera de tipo string con los valores de asignar , sugerir y rechazar
-    //     //aqui se haran las modificaciones con un if then else
-    //     //tambien se tendra que enviar a la vista un tipo de identificador para hacer 
-    //     //los diferentes tipos de mensaje
-
-    //     // dd($request->all());
-    //     $idReserva = $request->input('idReserva');
-
-    //     $checkboxValue = $request->input('checkboxValues');
-
-    //     $tipoSeleccionado = $request->input('tipoSeleccionado');
-
-    //     $menu = view('componentes/menu'); // Crear la vista del menú
-    //     // Verificar el valor de tipoSeleccionado y redirigir a diferentes vistas
-    //     if ($tipoSeleccionado == 'asignar') {
-    //         return view('mensajes.correo', compact('menu', 'idReserva', 'checkboxValues', 'tipoSeleccionado'));
-    //     }
-
-    //     // Verificar el valor de tipoSeleccionado y redirigir a diferentes vistas
-    //     if ($tipoSeleccionado == 'sugerir') {
-    //         return view('mensajes.correo', compact('menu', 'idReserva', 'checkboxValues', 'tipoSeleccionado'));
-    //     }
-
-    //     // Verificar el valor de tipoSeleccionado y redirigir a diferentes vistas
-    //     if ($tipoSeleccionado == 'rechazar') {
-    //         return view('mensajes.correo', compact('menu', 'idReserva', 'checkboxValues', 'tipoSeleccionado'));
-    //     }
-
-
-
-    //     // return view('mensajes.correo', compact('menu', 'idReserva', 'checkboxValue', 'tipoSeleccionado'));
-    // }
-
+    
     public function enviarCorreo(Request $request)
     {
         // Recuperar los datos del request
