64eee3d0f1cabe01af468c689546739f0f327d05
MadaiFlores <madaifloresmodragon@gmail.com>
2024-05-25 16:06:43 -0400

    icono de notificaciones

--- resources/views/componentes/menu.blade.php
+++ resources/views/componentes/menu.blade.php
@@ -4,6 +4,11 @@
 <body id="body-pd">
     <header class="header" id="header">
         <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+        <div class="notificaciones">
+            <a href="#" class="notificaciones-link">
+                <i class="bi bi-bell-fill"></i>
+            </a>
+        </div>
         <!-- <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div> -->
     </header>
     <div class="l-navbar show-menu" id="nav-bar">
