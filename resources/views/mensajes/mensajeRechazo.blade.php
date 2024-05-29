815ff24138e50fe469dcbae12412f6da41505d1e
Andres Bravo <BravoAndres@gmail.com>
2024-05-29 13:36:55 -0400

    CorreoRechazoReservaCompleto

--- resources/views/mensajes/mensajeRechazo.blade.php
+++ resources/views/mensajes/mensajeRechazo.blade.php
@@ -1,5 +1,24 @@
 <!DOCTYPE html>
 <html lang="es">
+
+<?php
+use App\Models\Ambientes;
+// use App\Models\Reservas;
+// use App\Models\Usuarios;
+// use App\Models\Motivos;
+// use App\Models\MateriasSeleccionado;
+use App\Models\Materias;
+use App\Models\PeriodosSeleccionado;
+use App\Models\Periodos;
+use App\Models\ReservasAmbiente;
+use App\Models\NombreAmbientes;
+use App\Models\TipoAmbientes;
+use App\Models\DocentesMaterias;
+use Carbon\Carbon; // Asegúrate de usar Carbon para manipular fechas fácilmente
+
+// Establecer la zona horaria predeterminada
+date_default_timezone_set('America/La_Paz');
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -107,22 +126,74 @@
     use App\Models\Reservas;
     use App\Models\Usuarios;
     use App\Models\Motivos;
+    use App\Models\MateriasSeleccionado;
     $reserva = Reservas::find($idReserva); //extraemos la reserva actual
     $idDocente = $reserva->docentes_id;
     $DocenteAux = Usuarios::find($idDocente);
+    $tipoReserva = $reserva->Tipo ; //vamos a mostrar el tipo de la reserva
 
     $MotivoId = $reserva->motivos_id;
     $motivo = Motivos::find($MotivoId); //encontramos el motivo de la reserva
-    // dd($motivo);
-    // dd($DocenteAux);
-    // dd($DocenteAux->name);
-?>
+    
+    $registrosMatSelec = MateriasSeleccionado::where('reservas_id',$idReserva)->get();
+    // dd($registrosMatSelec);
+    $idMateria = $registrosMatSelec[0]->materias_id;
+    $registroMateria = Materias::where('id',$idMateria)->first();
+    $nombreMateria = $registroMateria->Nombre;    
+    // dd($nombreMateria);
+
+    // vamos a ver los peridos seleccionados
+    $periodosSeleccionados = PeriodosSeleccionado::where('reservas_id',$idReserva)->get();
+    $tamPeriodosSeleccionado = count($periodosSeleccionados);
+    // dd($tamPeriodoSelec);
+
+    if($tamPeriodosSeleccionado == 1){
+        $periodoId = $periodosSeleccionados[0]->periodos_id;
+                    
+        $periodoBuscar = Periodos :: where('id',$periodoId)->first();
+        $periodo = $periodoBuscar->HoraIntervalo;
+        $partes_P = explode('-', $periodo);
+                    
+        $horaInicio = trim(str_replace(' ', '', $partes_P[0]));
+        $horaFin = trim(str_replace(' ', '', $partes_P[1]));
+                
+    }
+    else{
+
+        $periodoId = $periodosSeleccionados[0]->periodos_id;
+        $periodoId2 = $periodosSeleccionados[1]->periodos_id;
+
+        $periodoBuscar = Periodos :: where('id',$periodoId)->first();     
+        $periodoBuscar2 = Periodos :: where('id',$periodoId2)->first();
+
+        $periodo = $periodoBuscar->HoraIntervalo;
+        $periodo2 = $periodoBuscar2->HoraIntervalo;
+                    
+        $partes_P = explode('-', $periodo);
+        $partes_P2 = explode('-', $periodo2);
+        //dd($partes_P,$partes_P2);
+
+        $horaInicio = trim(str_replace(' ', '', $partes_P[0]));
+        $horaFin = trim(str_replace(' ', '', $partes_P2[1]));
+        }
+
+        
+    ?>
 
 <body>
     <div class="container">
+        <?php
+            // Obtener la fecha y hora actual en la zona horaria de Bolivia
+            $fechaEnvio = Carbon::now('America/La_Paz');
+
+            // Formatear la fecha de envío
+            $fechaEnvioFormateada = $fechaEnvio->locale('es')->isoFormat('dddd, D [de] MMMM');
+            $horaEnvio = $fechaEnvio->format('H:i');
+        ?>
+
         <header class="header">
             <h1>Rechazo de solicitud de reserva</h1>
-            <p>Sábado, 18 de mayo (hace 17 horas)</p>
+            <p><?php echo $fechaEnvioFormateada; ?> (a las <?php echo $horaEnvio; ?>)</p>
         </header>
         <main class="main-content">
             <div class="centered-text">
@@ -130,7 +201,7 @@
                 <p>¡Esperamos que este mensaje te encuentre muy bien! Te escribimos desde el Sistema de Reservas FCyT. Se informa que su solicitud de reserva ha sido Rechazada.</p>
             </div>
             <section class="reservation-details">
-                <h2>Detalle de reserva ({{$reserva->Tipo}}):</h2>
+                <h2>Detalle de reserva ({{$tipoReserva}}):</h2>
                 <table>
                     <tr>
                         <td>Nombre docente:</td>
@@ -150,7 +221,7 @@
                     </tr>
                     <tr>
                         <td>Periodo:</td>
-                        <td>8:15 - 9:45</td>
+                        <td>{{ $horaInicio }} - {{ $horaFin }}</td>
                     </tr>
                     <tr>
                         <td>Tipo de ambiente:</td>
@@ -158,12 +229,45 @@
                     </tr>
                     <tr>
                         <td>Materia:</td>
-                        <td>Elementos de programación</td>
+                        <td>{{$nombreMateria}}</td>
                     </tr>
                     <tr>
                         <!-- SI ES GRUPAL AUMENTAR EL NOMBRE DEL DOCENTE A LADO DEL GRUPO -->
                         <td>Grupo(s):</td>
-                        <td>1, 2</td>
+                        {{-- <td>1, 2</td> --}}
+                        <?php
+                                $gruposMateria = [];
+                            ?>
+                            @if ($tipoReserva == "individual")
+                                @foreach ($registrosMatSelec as $registroMatSelec)
+                                    <?php
+                                        $idMateria = $registroMatSelec->materias_id;
+                                        $materia = Materias::where('id',$idMateria)->first();
+                                        $gruposMateria[] = $materia->Grupo;
+                                    ?>
+                                @endforeach
+                                <td>{{ implode(', ', $gruposMateria) }}</td>
+                            @else
+                                @foreach ($registrosMatSelec as $registroMatSelec)
+                                    <?php
+                                        $idMateria = $registroMatSelec->materias_id;
+                                        $materia = Materias::where('id',$idMateria)->first();
+                                        
+                                        $registroDocMat = DocentesMaterias::where('materias_id',$idMateria)->first();
+                                        $idDocente = $registroDocMat->docentes_id;
+                                        $registroDocente = Usuarios::where('id',$idDocente)->first();
+                                        $nombreDocente = $registroDocente->name;
+                                        
+                                        $grupo = $materia->Grupo;
+                                        $gruposMateria[] = "$grupo, $nombreDocente";
+                                    ?>
+                                @endforeach              
+                                <td>
+                                    @foreach($gruposMateria as $entry)
+                                        {{ $entry }}<br>
+                                    @endforeach
+                                </td>
+                            @endif
                         <!-- <td>1, Leticia Coca Blanco<br>3, Vladimir Costas</td> ASI SE TENDRIA QUE VER -->
                     </tr>
                 </table>
