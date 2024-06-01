dd9f49cb4afd3401cb930f25fc8f7087b505490f
David Arcayne <arcayned6075@gmail.com>
2024-06-01 01:16:07 -0900

    mostrar detalle notificacion SUGERENCIA- DOCENTE

--- resources/views/notificaciones/sugerencia.blade.php
+++ resources/views/notificaciones/sugerencia.blade.php
@@ -2,6 +2,37 @@
 
 @section('notificaciones/sugerencia')
 {{-- {{ dd(get_defined_vars()) }} --}}
+
+<?php
+    //  notificacionId y reservaId
+    use App\Models\UsuariosNotificacion;
+    use App\Models\Notificaciones;
+    use App\Models\Reservas;
+    use App\Models\Motivos;
+    use App\Models\MateriasSeleccionado;
+    use App\Models\Materias;
+    use App\Models\PeriodosSeleccionado;
+    use App\Models\Periodos;
+    use App\Models\Usuarios;
+    use App\Models\DocentesMaterias;
+    use App\Models\ReservasAmbiente;
+    use App\Models\Ambientes;
+    use App\Models\NombreAmbientes;
+    use App\Models\TipoAmbientes;
+    use Carbon\Carbon; // Asegúrate de usar Carbon para manipular fechas fácilmente
+
+    $timezone = 'America/La_Paz';
+
+    $notificacion = Notificaciones::where('id', $notificacionId)->first();
+
+    $fecha = Carbon::parse($notificacion->fecha_actual_sistema)->setTimezone('America/La_Paz'); // Convertimos a la zona horaria correcta
+    $fechaActual = Carbon::now('America/La_Paz'); // Nos aseguramos que ambas fechas estén en la misma zona horaria
+
+    $fechaFormateada = $fecha->locale('es')->isoFormat('dddd, D [de] MMMM');
+    $diferencia = $fecha->diffForHumans($fechaActual);
+    // dd($diferencia);
+
+?>
 <div class="container mt-3">
     <div class="card vercard">
         <!-- ASUNTO DE LA SUGERENCIA -->
@@ -20,50 +51,183 @@
             <hr>
             <div class="detalleReserva">
                 <!-- DETALLE DE RESERVA -->
-                <label for="detalle" class="col-form-label" style="font-weight: bold;">Detalle reserva (Poner si es individual o grupal):</label>
-                <hr>
-                <div class="tabla-Detalle">
-                    <table class="table table-borderless">
-                        <tbody>
-                            <tr>
-                                <td style="width: 50%;">Nombre docente</td>
-                                <td style="width: 50%;">Leticia Blanco Coca</td>
-                            </tr>
-                            <tr>
-                                <td style="width: 50%;">Cantidad de estudiantes</td>
-                                <td style="width: 50%;">85</td>
-                            </tr>
-                            <tr>
-                                <td style="width: 50%;">Motivo reserva</td>
-                                <td style="width: 50%;">Examen primer parcial</td>
-                            </tr>
-                            <tr>
-                                <td style="width: 50%;">Fecha</td>
-                                <td style="width: 50%;">05-06-2024</td>
-                            </tr>
-                            <tr>
-                                <td style="width: 50%;">Periodo</td>
-                                <td style="width: 50%;">08:15-09:45</td>
-                            </tr>
-                            <tr>
-                                <td style="width: 50%;">Tipo de ambiente</td>
-                                <td style="width: 50%;">Aula comun</td>
-                            </tr>
-                            <tr>
-                                <td style="width: 50%;">Materia</td>
-                                <td style="width: 50%;">Elemento de programación</td>
-                            </tr>
-                            <tr>
-                                <td style="width: 50%;">Grupo(s)</td>
-                                <td style="width: 50%;">1, 2, si es grupal poner el nombre del docente y su grupo aqui</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                <?php
+                $registroUN = UsuariosNotificacion::where('notificaciones_id',$notificacionId)->first();
+                $docenteId = $registroUN->usuarios_id;
+                $registroDocente = Usuarios::where('id',$docenteId)->first();
+                $nombreDocente = $registroDocente->name;
+
+                $registroReserva = Reservas::where('id',$reservaId)->first();
+                $cantidadEstudiante = $registroReserva->CantEstudiante;
+
+                $tipoReserva = $registroReserva->Tipo ;
+
+                $motivoId = $registroReserva->motivos_id;
+                $registroMotivo = Motivos::where('id',$motivoId)->first();
+                $motivo = $registroMotivo->Nombre;
+
+                $fechaReserva = $registroReserva->fecha;
+
+                $tipoAmbiente = $registroReserva->TipoAmbiente;
+                 //  verificar
+                $registrosMatSelec = MateriasSeleccionado::where('reservas_id',$reservaId)->get();
+                 //   $tamañoMatSelec = count($regiatrosMatSelec);
+                $idMateria = $registrosMatSelec[0]->materias_id;
+
+                $registroMateria = Materias::where('id',$idMateria)->first();
+                $nombreMateria = $registroMateria->Nombre;
+
+                $periodosSeleccionados = PeriodosSeleccionado::where('reservas_id',$reservaId)->get();
+                $tamPeriodosSeleccionado = count($periodosSeleccionados);
+                 // dd($tamPeriodoSelec);
+
+                if($tamPeriodosSeleccionado == 1){
+                     $periodoId = $periodosSeleccionados[0]->periodos_id;
+                     
+                     $periodoBuscar = Periodos :: where('id',$periodoId)->first();
+                     $periodo = $periodoBuscar->HoraIntervalo;
+                     $partes_P = explode('-', $periodo);
+                     
+                     $horaInicio = trim(str_replace(' ', '', $partes_P[0]));
+                     $horaFin = trim(str_replace(' ', '', $partes_P[1]));
+                     
+                 }else{
+
+                     $periodoId = $periodosSeleccionados[0]->periodos_id;
+                     $periodoId2 = $periodosSeleccionados[1]->periodos_id;
+
+                     $periodoBuscar = Periodos :: where('id',$periodoId)->first();     
+                     $periodoBuscar2 = Periodos :: where('id',$periodoId2)->first();
+
+                     $periodo = $periodoBuscar->HoraIntervalo;
+                     $periodo2 = $periodoBuscar2->HoraIntervalo;
+                     
+                     $partes_P = explode('-', $periodo);
+                     $partes_P2 = explode('-', $periodo2);
+                     //dd($partes_P,$partes_P2);
+
+                     $horaInicio = trim(str_replace(' ', '', $partes_P[0]));
+                     $horaFin = trim(str_replace(' ', '', $partes_P2[1]));
+                     
+                     // if($i==1){dd($horaInicio,$horaFin);}
+                 
+                 }
+
+         ?>
+
+            <label for="detalle" class="col-form-label" style="font-weight: bold;">Detalle reserva ({{ $tipoReserva }}):</label>
+            <hr>
+            <div class="tabla-Detalle">
+                
+                <table class="table table-borderless">
+                    <tbody>
+                        <tr>
+                            <td style="width: 50%;">Nombre docente</td>
+                            <td style="width: 50%;">{{ $nombreDocente }}</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%;">Cantidad de estudiantes</td>
+                            <td style="width: 50%;">{{ $cantidadEstudiante }}</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%;">Motivo reserva</td>
+                            <td style="width: 50%;">{{ $motivo }}</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%;">Fecha</td>
+                            <td style="width: 50%;">{{ $fechaReserva }}</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%;">Periodo</td>
+                            <td style="width: 50%;">{{ $horaInicio }} - {{ $horaFin }}</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%;">Tipo de ambiente</td>
+                            <td style="width: 50%;">{{ $tipoAmbiente }}</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%;">Materia</td>
+                            <td style="width: 50%;">{{ $nombreMateria }}</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%;">Grupo(s)</td>
+                            <?php
+                                $gruposMateria = [];
+                            ?>
+                            @if ($tipoReserva == "individual")
+                                @foreach ($registrosMatSelec as $registroMatSelec)
+                                    <?php
+                                        $idMateria = $registroMatSelec->materias_id;
+                                        $materia = Materias::where('id',$idMateria)->first();
+                                        $gruposMateria[] = $materia->Grupo;
+                                    ?>
+                                @endforeach
+                            
+                                <td>{{ implode(', ', $gruposMateria) }}</td>
+                            @else
+                                @foreach ($registrosMatSelec as $registroMatSelec)
+                                    <?php
+                                        $idMateria = $registroMatSelec->materias_id;
+                                        $materia = Materias::where('id',$idMateria)->first();
+                                        
+                                        $registroDocMat = DocentesMaterias::where('materias_id',$idMateria)->first();
+                                        $idDocente = $registroDocMat->docentes_id;
+                                        $registroDocente = Usuarios::where('id',$idDocente)->first();
+                                        $nombreDocente = $registroDocente->name;
+                                        
+                                        $grupo = $materia->Grupo;
+                                        $gruposMateria[] = "$grupo, $nombreDocente";
+                                    ?>
+                                @endforeach              
+                                <td>
+                                    @foreach($gruposMateria as $entry)
+                                        {{ $entry }}<br>
+                                    @endforeach
+                                </td>
+                            @endif
+                    
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             </div>
             <hr>
             <div class="detalleambiente">
                 <!-- DETALLE DE AMBIENTE SUGERIDO -->
+
+                <?php
+                $registrosRA = ReservasAmbiente::where('reservas_id',$reservaId)->get();
+                // Inicializar arrays para almacenar los datos
+                $nombresAmbientes = [];
+                $capacidadesAmbientes = [];
+                $ubicacionesAmbientes = [];
+                $tiposAmbientes = [];
+                
+                // Iterar sobre cada registro y obtener los detalles correspondientes
+                foreach ($registrosRA as $registroRA) {
+                    $idAmbiente = $registroRA->ambientes_id;
+                    $registroAmbiente = Ambientes::where('id', $idAmbiente)->first();
+                
+                    $idNombreAmb = $registroAmbiente->nombre_ambientes_id;
+                    $registroNombreAmb = NombreAmbientes::where('id', $idNombreAmb)->first();
+                    $nombreAmbiente = $registroNombreAmb->Nombre;
+                    $nombresAmbientes[] = $nombreAmbiente;
+                
+                    $capacidadAmbiente = $registroAmbiente->Capacidad;
+                    $capacidadesAmbientes[] = $capacidadAmbiente;
+                
+                    $ubicacionAmbiente = $registroAmbiente->Ubicacion;
+                    $ubicacionesAmbientes[] = $ubicacionAmbiente;
+                
+                    $idTipoAmb = $registroAmbiente->tipo_ambientes_id;
+                    $registroTipoAmb = TipoAmbientes::where('id', $idTipoAmb)->first();
+                    $tipoAmbiente = $registroTipoAmb->Nombre;
+                    $tiposAmbientes[] = $tipoAmbiente;
+                }
+// dd($nombresAmbientes, $capacidadesAmbientes,$ubicacionesAmbientes, $tiposAmbientes);
+            ?>
+
                 <label for="detalle" class="col-form-label" style="font-weight: bold;">Detalle de sugerencia de ambiente:</label>
                 <hr>
                 <div class="tabladetalleambiente" style="max-height: 200px; overflow-y: auto;">
@@ -80,20 +244,20 @@
                         </thead>
                         <tbody>
                             <tr>
-                            <th>690 A</th>
-                            <td>50</td>
-                            <td>Edificio nuevo</td>
-                            <td>Aula comun</td>
-                            <td>08:15 - 09:45</td>
-                            <td>05-06-2024</td>
+                            <th>{{$nombresAmbientes[0]}}</th>
+                            <td>{{ $capacidadesAmbientes[0]}}</td>
+                            <td>{{$ubicacionesAmbientes[0]}}</td>
+                            <td>{{ $tiposAmbientes[0]}}</td>
+                            <td>{{ $horaInicio }} - {{ $horaFin }}</td>
+                            <td>{{ $fecha }}</td>
                             </tr>
                             <tr>
-                            <th>690 B</th>
-                            <td>50</td>
-                            <td>Edificio nuevo</td>
-                            <td>Aula comun</td>
-                            <td>08:15 - 09:45</td>
-                            <td>05-06-2024</td>
+                                <th>{{$nombresAmbientes[1]}}</th>
+                                <td>{{ $capacidadesAmbientes[1]}}</td>
+                                <td>{{$ubicacionesAmbientes[1]}}</td>
+                                <td>{{ $tiposAmbientes[1]}}</td>
+                                <td>{{ $horaInicio }} - {{ $horaFin }}</td>
+                                <td>{{ $fecha }}</td>
                             </tr>
                         </tbody>
                     </table>
