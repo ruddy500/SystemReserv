2eef39f4651b7fb6263281fa31444543ad556250
AndruQM <quispemoyaa@gmail.com>
2024-06-08 17:59:37 -0400

    fullcalendar agregado

--- resources/views/calendario/inicio.blade.php
+++ resources/views/calendario/inicio.blade.php
@@ -1,7 +1,23 @@
 @extends('calendario/principal')
 
 @section('contenido-inicio')
-<div class="card-body bg-content" style="border-radius: 5px;">
-    <p>este es la vista de inicio calendario</p>
+<div class="container">
+        <!-- Contenedor donde se mostrará el calendario -->
+        <div id='calendar'></div>
 </div>
+<!-- sin este script no se podra visulizar el calendario POR LO QUE ES NECESARIO-->
+<script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                locale: 'es', // Para mostrar el calendario en español
+                events: [
+                    { title: 'Evento 1', start: '2024-06-01' },
+                    { title: 'Evento 2', start: '2024-06-07', end: '2024-06-10' },
+                ]//aqui se crean los eventos 
+            });
+            calendar.render();
+        });
+    </script>
 @endsection
