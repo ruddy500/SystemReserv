1d0e2bf908656eab23322b3c09d1f3c548ba8083
jonathanalexiiz <jonathanasda@gmail.com>
2024-06-09 22:51:21 -0400

    tabla anuncios añadido

---
+++ database/migrations/2024_06_10_024803_create_anuncios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnunciosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anuncios', function (Blueprint $table) {
+            $table->id();
+            $table->string('Titulo',100)->nullable();
+            $table->text('Contenido')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anuncios');
+    }
+}
