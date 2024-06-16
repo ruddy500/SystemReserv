fb412678bdcc0dea9e1d74df5a09992b7dea9193
Andres Bravo <BravoAndres@gmail.com>
2024-06-16 08:09:57 -0400

    AumentarAulas

--- database/seeders/NombreAmbientesTableSeeder.php
+++ database/seeders/NombreAmbientesTableSeeder.php
@@ -9,7 +9,7 @@ class NombreAmbientesTableSeeder extends Seeder
 {
     public function run()
     {
-        $nombreAmbientes = ['690 A','690 B','690 C','690 D','691 A','691 B','691 C','691 D','691 E','692 A','692 B','692 C','692 D','692 E','693 A','693 B','693 C','693 D','693 E'];
+        $nombreAmbientes = ['690 A','690 B','690 C','690 D','691 A','691 B','691 C','691 D','691 E','692 A','692 B','692 C','692 D','692 E','693 A','693 B','693 C','693 D','693 E','Auditorio','660','661'];
             // Agrega aquí más ambientes si los necesitas
 
         foreach ($nombreAmbientes as $nombreAmbiente) {
